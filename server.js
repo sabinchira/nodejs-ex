25874a59442fe0d5383825d44b2ab7d1e196bbf3
CHIRA  Victor <VChira@iss.local>
2017-08-15 14:43:15 +0300

    provide frecventa.html

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/frecventa', function(req,res){
+  res.render('frequency.html');
+});
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
